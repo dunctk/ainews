2b9e98436560fa975367a8393ea47cdf856b2a5a
Duncan Felix <d@uncan.net>
2024-11-08 08:46:38 +0100

    x

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-python manage.py migrate
-python manage.py collectstatic --no-input
+uv run manage.py migrate
+uv run manage.py collectstatic --no-input
 
 granian --interface asgi --host 0.0.0.0 --port 80 --workers 4 curator.asgi:application
