719e7c162ec9c283005d419efd5a52fa6df2369e
Duncan Felix <d@uncan.net>
2024-11-20 13:38:06 +0100

    x

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import HomeView, PostListView
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from .views import HomeView, PostListView, PostViewSet
 
 app_name = 'core'
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('posts/', PostListView.as_view(), name='post_list'),
+    path('api/', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,23 @@
-
-# Create your views here.
 from django.views.generic import ListView
 from .models import Post, Story
+from rest_framework import viewsets
+from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
+
+# First, create a serializer for your Post model
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'  # This will include all model fields
+        # Or specify only the fields that exist in your model, for example:
+        # fields = ['id', 'body', 'created']  # adjust these based on your actual model fields
+
+# Add the ViewSet
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    pagination_class = PageNumberPagination
+    page_size = 20
 
 class HomeView(ListView):
     model = Story
@@ -16,3 +32,5 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-created_at']
     paginate_by = 20
+
+
