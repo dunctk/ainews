79ba02c828f953100504c5dc730b902d56717c99
Duncan Felix <d@uncan.net>
2024-11-20 13:08:29 +0100

    x

--- core/management/commands/run.py
+++ core/management/commands/run.py
@@ -1,9 +1,15 @@
-from core.tasks import generate_post_for_all_stories, get_stories, process_stories
+from core.tasks import generate_post_for_all_stories, get_stories, process_stories, sync_sitemap
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = 'Runs the relevant page finder, post creator and post content generator'
 
     def handle(self, *args, **kwargs):
+        # First sync the sitemap
+        self.stdout.write('Syncing sitemap...')
+        sync_sitemap()
+        
+        # Then process stories and generate posts
+        self.stdout.write('Processing stories...')
         process_stories(get_stories())
         generate_post_for_all_stories()
