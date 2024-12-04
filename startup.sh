01221a8c8a4a1690fe2e634a7248e1ad847c6c86
Duncan Felix <d@uncan.net>
2024-12-04 13:53:26 +0100

    x

--- startup.sh
+++ startup.sh
@@ -3,4 +3,7 @@ set -e
 uv run manage.py migrate
 uv run manage.py collectstatic --no-input
 
+uv run manage.py scrape      
+
+uv run manage.py 
 uv run granian --interface asgi --host 0.0.0.0 --port 80 --workers 4 curator.asgi:application
